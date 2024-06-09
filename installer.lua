dce2cfd63ab429c3f8c8d14ba3161a2a6443695c
petdono <89791014+petdono@users.noreply.github.com>
2024-06-09 00:01:52 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,1 +1,3 @@
 shell.run("wget run https://basalt.madefor.cc/install.lua packed basalt.lua master") --Install Basalt
+shell.run("https://raw.githubusercontent.com/petdono/xxfurnace-cc/main/xxfurnace.lua") --Install UI
+shell.run("https://raw.githubusercontent.com/petdono/xxfurnace-cc/main/ee-ironchunk.lua") --Install EE: IronChunk module
